0ade0cee6830a385a9d92ed22ae46d7d91d7ce08
CristianPere <cristianperezcastillo.98@gmail.com>
2022-02-16 08:13:28 +0100

    despuesTesting

---
+++ tests/Feature/GruposUsuarioTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Grupo;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Laravel\Sanctum\Sanctum;
+use Tests\TestCase;
+
+class GruposUsuarioTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_UserGrupos()
+    {
+        Sanctum::actingAs(
+            User::factory()
+            ->hasAttached(
+                Grupo::factory()->count(3)
+            )
+            ->create()
+        );
+        $response = $this->get('/api/grupos');
+        $response->assertStatus(200)
+        ->assertJsonStructure(['data' => [['curso', 'letra', 'nivel']]])
+        ->assertJsonCount(3, 'data');
+    }
+}
